1ee171026c81298c9685284f07f1bafa7e8f7cc1
Immanuel <immanuel1immanuel@gmail.com>
2024-08-19 01:46:46 -0700

    alogo

---
+++ index.js
@@ -0,0 +1,19 @@
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];  // The element to be inserted into the sorted part
+        let j = i - 1;
+
+           while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+
+     
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+
+
+let arr = [12, 11, 13, 5, 6];
+console.log("Sorted array:", insertionSort(arr));
